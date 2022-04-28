53564bf700027549a892c4271447d4131d070204
Crothgeb <chrisrothgeb@gmail.com>
2022-04-27 21:03:08 -0400

    Initial Setup

---
+++ components/MainComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native';
+import { Icon } from 'react-native-elements';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
+import { connect } from 'react-redux';
+
+export default Main;
---
+++ redux/configureStore.js
@@ -0,0 +1,22 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: false
+}
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        persistCombineReducers(config, {}),
+        applyMiddleware(thunk, logger)
+    );
+
+    const persistor = persistStore(store);
+
+    return { persistor, store };
+}
